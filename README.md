00889bf0453e58947048a9c642bca0eba22de7e3
lucotmo <lucotmo@gmail.com>
2019-03-16 16:35:59 -0500

    inicio

---
+++ README.md
@@ -0,0 +1,3 @@
+## Workflow webpack
+
+Flujo de trabajo para trabajar con webpack
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import style from "./scss/main.scss"
+import logo from "./img/lucotmo-logo.png"
+
+console.log('probando')
