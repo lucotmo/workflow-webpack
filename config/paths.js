95c5b877847db7b51caeeb5645f4d9ef45d6387d
lucotmo <lucotmo@gmail.com>
2019-11-25 21:20:37 -0500

    project structure

--- config/paths.js
+++ config/paths.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   srcDir: path.resolve(__dirname, '../', 'src'),
-  publicDir: path.resolve(__dirname, '../', 'public'),
+  publicDir: path.resolve(__dirname, '../', 'dist'),
   imagesFolder: 'images',
   fontsFolder: 'fonts',
   cssFolder: 'css',
